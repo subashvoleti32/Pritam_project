3ad94feb094325c7ed63800ffe734c77240968de
subash voleti <31156606+subashvoleti32@users.noreply.github.com>
2023-10-26 22:19:35 +0530

    Delete database.py

--- database.py
+++
@@ -1,6 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
-engine = create_engine('sqlite:///customer.db', echo = True)
-SessionLocal=sessionmaker(bind=engine)
-Base = declarative_base()
